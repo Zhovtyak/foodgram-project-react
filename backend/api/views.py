912d7ebdcbbbb5d0bf144aaff632e52ff401f9d9
Zhovtyak <jasdef124@gmail.com>
2023-09-26 17:50:34 +0300

    PEP8 fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -144,8 +144,7 @@ class ReceiptViewSet(viewsets.ModelViewSet):
                                                       'recipe': recipe.id})
             if serializer.is_valid():
                 serializer.save()
-                return Response(
-                                status=status.HTTP_201_CREATED)
+                return Response(status=status.HTTP_201_CREATED)
             return Response(serializer.errors,
                             status=status.HTTP_400_BAD_REQUEST)
 
